97d6ac3f68f015fcef1d0d5a1c1b595426ddd7ee
mmmeyers <marsi286@gmail.com>
2017-02-25 15:35:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const app = "I don't do much."
+const products = [
+  { name: 'Head & Shoulders Shampoo', standardPrice: 4.99, discount: .6 },
+  { name: 'Twinkies', standardPrice: 7.99, discount: .45 },
+  { name: 'Oreos', standardPrice: 6.49, discount: .8 },
+  { name: 'Jasmine-scented bath pearls', standardPrice: 13.99, discount: .7 },
+];
+
+function reduce(collection, callback, initialValue) {
+  let result = initialValue;
+
+  collection.forEach((product, index) => {
+    result = callback(result, product, index, collection);
+  });
+
+  return result;
+}
